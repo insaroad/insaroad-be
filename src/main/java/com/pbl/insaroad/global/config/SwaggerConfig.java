988cafa0dd70cf86c6fa60506a3d3c97eba27a01
김나경 <1030n@naver.com>
2025-12-19 16:20:55 +0900

    :wrench: Settings: SwaggerConfig 추가

---
+++ src/main/java/com/pbl/insaroad/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.pbl.insaroad.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info =
+    @Info(
+        title = "🐾 Insaroad 명세서",
+        contact =
+        @Contact(name = "Insaroad", url = "https://danchu.site", email = "1030n@naver.com")),
+    security = @SecurityRequirement(name = "Authorization"),
+    servers = {
+        @Server(url = "https://api.danchu.site", description = "🚀 운영 서버"),
+        @Server(url = "http://localhost:8080", description = "🛠️ 로컬 서버")
+    })
+@SecurityScheme(
+    name = "Authorization",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT")
+public class SwaggerConfig {
+
+  @Bean
+  public GroupedOpenApi publicApi() {
+    return GroupedOpenApi.builder()
+        .group("Swagger API") // API 그룹명
+        .pathsToMatch("/api/**", "/swagger-ui/**", "/v3/api-docs/**")
+        .build();
+  }
+}
